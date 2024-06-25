3368269de917966f0e010da8114bb1c9d33309d0
OmkarTidke8080 <omkartidke8080@gmail.com>
2024-06-25 17:06:18 +0530

    use registration api added

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    await mongoose.connect(`${process.env.MONGO_URI}`);
     console.log(`\n Mongo DB connected !!! `);
   } catch (error) {
     console.log("MONGO DB connection failed", error);
